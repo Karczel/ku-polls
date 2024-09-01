166aa421901caeef22ba2aa0c178f343d8ef96d7
Karczel <92095164+Karczel@users.noreply.github.com>
2024-09-01 11:56:15 +0700

    migrate

--- polls/models.py
+++ polls/models.py
@@ -7,7 +7,7 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published',default=timezone.now)
-    end_date = models.DataTimeField('end date', null=True, blank=True)
+    end_date = models.DateTimeField('end date', null=True, blank=True)
 
     def __str__(self):
         return self.question_text
