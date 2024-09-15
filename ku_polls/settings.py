5448aaafe2139967acff1d21b5f9a88d0b587d13
Karczel <92095164+Karczel@users.noreply.github.com>
2024-09-15 16:02:03 +0700

    DEBUG=False version

--- ku_polls/settings.py
+++ ku_polls/settings.py
@@ -113,8 +113,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'polls/static/'
-
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Message storage
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
