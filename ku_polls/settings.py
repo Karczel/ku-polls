d69e9eb0018a70073085ff90458fa76f8329e1e4
Karczel <92095164+Karczel@users.noreply.github.com>
2024-09-15 15:48:41 +0700

    Set Debug to true to fix style.css load error temporarily

--- ku_polls/settings.py
+++ ku_polls/settings.py
@@ -113,7 +113,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'polls/static/'
+
 
 # Message storage
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
